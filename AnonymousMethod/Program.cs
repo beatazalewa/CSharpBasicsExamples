4e0898a7b46167cc572b783835d3a03aa32c4fa1
Daria Zalewa <zalewa.daria@gmail.com>
2023-04-21 20:16:46 +0200

    Add project files.

---
+++ AnonymousMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnonymousMethod // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public delegate void Print(int value);
+        static void Main(string[] args)
+        {
+            Print print = delegate (int val) {
+                Console.WriteLine($"Inside Anonymous method. Value: {val}");
+            };
+            print(100);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpBasicsExamples // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public delegate void Print(int value);
+        static void Main(string[] args)
+        {
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Anonymous method");
+            Console.Write("Your option? ");
+        }
+    }
+}
