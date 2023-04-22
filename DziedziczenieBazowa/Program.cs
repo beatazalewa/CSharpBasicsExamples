8bfb0a29be53df6ef3a1428f26de7095676a5ad3
beatazalewa <info@zalnet.pl>
2023-04-22 12:28:54 +0200

    Added Dziedziczenie Bazowa project

---
+++ DziedziczenieBazowa/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace DziedziczenieBazowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Blat tp = new Blat(4, 5);
+            tp.WyswietInformacje();
+            Console.ReadKey();
+
+            // Wynik działania programu
+            //Dlugosc: 4
+            //Szerokosc: 5
+            //Powierzchnia: 20
+            //Koszt: 1000
+        }
+    }
+
+    // klasa bazowa
+    class Prostokat
+    {
+        protected int dlugosc;
+        protected int szerokosc;
+
+        public Prostokat(int d, int s)
+        {
+            dlugosc = d;
+            szerokosc = s;
+        }
+
+        public int ObliczPowierzchnie()
+        {
+            return dlugosc * szerokosc;
+        }
+
+        public void WyswietlInformacje()
+        {
+            Console.WriteLine($"Długość: {dlugosc}");
+            Console.WriteLine($"Szerokość: {szerokosc}");
+            Console.WriteLine($"Powierzchnia: {ObliczPowierzchnie}");
+        }
+    }
+
+    // klasa pochodna
+    class Blat : Prostokat
+    {
+        // Slowo kluczowe base przy konstruktorze pozwala nam wywowolac konsturktor klasy bazowej
+        // W tym momencie przekazalismy nasze parametry do konstruktora klasy bazowej
+        public Blat(int d, int s) : base(d, s)
+        {
+        }
+
+        public int Koszt()
+        {
+            int koszt;
+            koszt = ObliczPowierzchnie() * 50;
+            return koszt;
+        }
+
+        public void WyswietInformacje()
+        {
+            /* slowo kluczowe base pozwala nam odwolac sie do skladowych klasy bazowej
+            dla kompilatora wazniejsze sa zmienne z klasy, w ktorej wlasnie jestesmy
+            // za pomoca slowa kluczowego base wskazujemy jednoznacznie do ktorej skladowej chcemy się odwołlac. Dzieki  poniższemu wywolaniu w obecnej metodzie wywolamy
+              rowniez metode z klasy bazowej - wyswietlona zostanie wieksza ilosc informacji */
+            base.WyswietlInformacje();  // z klasy bazowej Prostokat
+            Console.WriteLine($"Koszt: {Koszt()}");  // z klasy pochodnej Blat
+        }
+
+    }
+}
