de3bf2d16ce23fd9e0407cea7f0d29bcbbf9c863
beatazalewa <info@zalnet.pl>
2023-04-22 08:12:18 +0200

    Added KlasaZKonstruktorem project

---
+++ KlasaZKonstruktorem/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace KlasaZKonstruktorem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // W momencie tworzenia nowego obiektu dojdzie do wywołania konstruktora
+            KlasaZKonstruktorem kzk = new KlasaZKonstruktorem();
+
+            Console.ReadKey();
+        }
+    }
+    class KlasaZKonstruktorem
+    {
+        private string tekst;
+        public KlasaZKonstruktorem()  //konstruktor domyślny
+        {
+            tekst = "Wywołanie konstruktora klasy";
+            Console.WriteLine(tekst);
+        }
+    }
+}
