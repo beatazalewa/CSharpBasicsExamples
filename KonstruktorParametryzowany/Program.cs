bda430198846617f56654246f5a1bc59b9b0cadf
beatazalewa <info@zalnet.pl>
2023-04-22 08:28:55 +0200

    Added KonstruktorParametryzowany project

---
+++ KonstruktorParametryzowany/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace KonstruktorParametryzowany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? marka = "";
+            string? model = "";
+            Console.WriteLine("Podaj markę samochodu: ");
+            marka = Console.ReadLine();
+            Console.WriteLine("Podaj model samochodu: ");
+            model = Console.ReadLine();
+            if (model == "")
+            {
+                KonstruktorParametryzowany kp = new KonstruktorParametryzowany(marka);
+            }
+            if (model != "" && marka != "")
+            {
+                KonstruktorParametryzowany kp2 = new KonstruktorParametryzowany(marka, model); 
+                
+            }
+            else
+            {
+                KonstruktorParametryzowany kp3 = new KonstruktorParametryzowany();
+            }          
+            Console.ReadKey();
+        }
+    }
+    class KonstruktorParametryzowany
+    {
+        private string marka;
+        private string model;
+
+        public KonstruktorParametryzowany()
+        {
+            marka = "BMW";
+            model = "X3";
+            Console.WriteLine($"Marka podana przez użytkownika to: {marka} model: {model}");
+        }
+        public KonstruktorParametryzowany(string mk)
+        {
+            marka = mk;
+            Console.WriteLine($"Marka podana przez użytkownika to: {marka}");
+        }
+        public KonstruktorParametryzowany(string mk, string ml)
+        {
+            marka = mk;
+            model = ml;
+            Console.WriteLine($"Marka podana przez użytkownika to: {marka} model: {model}");
+        }
+    }
+}
+
