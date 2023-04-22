1211a3961a5665785ee37d4e9fa5e0856954e49a
beatazalewa <info@zalnet.pl>
2023-04-22 14:06:51 +0200

    Added

---
+++ MetodaRozszerzajacaSygnatura/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MetodaRozszerzajacaSygnatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Pan Am";
+            int n = s.CompareTo("KLM");    // Wywolanie oryginalnej metody CompareTo
+            Console.WriteLine($"Result: {n}");
+            Console.ReadLine();
+        }
+    }
+}
+/* Idea pisania metody rozszerzenia polega na stworzeniu metody, ktora moze zostac wywolana w taki sam sposob, jak jedna z metod instancji klasy. Nie mozna zastapic istniejacej metody instancji przy uzyciu metody rozszerzenia, ale dodawac tylko nowe metody.
+*/
---
+++ MetodaRozszerzajacaSygnatura/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodaRozszerzajacaSygnatura
+{
+    static class StringExtensions
+    {
+        /* Nigdy sie nie wywola!!!
+
+Jesli utworzymy metode rozszerzenia, ktora ma dokladnie taki sam podpis jak istniejaca metoda instancji w klasie, kompilator nie bedzie narzekal na nasza nowa metode. Ale jesli sprobujemy wywolac taka metode, to metoda oryginalnej instancji zostanie wywolana, zamiast nasza metoda rozszerzenia.
+        */
+        public static int CompareTo(this string s1, string s2)
+        {
+            return 0;
+        }
+    }
+}
