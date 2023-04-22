262d48022ec8d85c7ca3d0361b8d387214e0acb8
beatazalewa <info@zalnet.pl>
2023-04-22 14:08:27 +0200

    Added MetodaRozszerzajaca project

---
+++ MetodaRozszerzajaca/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MetodaRozszerzajaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Pożar w fabryce zabawek";
+            Console.WriteLine(s.Decorate());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MetodaRozszerzajaca/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodaRozszerzajaca
+{
+    static class StringExtensions
+    {
+        public static string Decorate(this string s)
+        {
+            return "!!! " + s + " !!!";
+        }
+    }
+}
