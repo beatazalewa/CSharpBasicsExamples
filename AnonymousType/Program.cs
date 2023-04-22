33020ad41ca49f58b307a609c9bcf8c9032b6e54
Beata Zalewa <info@zalnet.pl>
2023-04-22 05:52:59 +0200

    Added Anonymous Type project

---
+++ AnonymousType/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AnonymousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var myAnonymousType = new
+            {
+                firstStringProperty = "First Property",
+                secondIntegerProperty = 2,
+                thirdBooleanProperty = true
+            };
+            /* myAnonymousType is an object of anonymous type created using the new keyword and object initializer syntax.
+             It includes three properties of different data types. 
+            */
+
+            DoSomething(myAnonymousType);
+            Console.ReadLine();
+        }
+
+        static void DoSomething(dynamic param)
+        {
+            Console.WriteLine(param.firstStringProperty);
+        }
+    }
+}
