b00edc2888a440e891af2aeb40e2d56f164096ea
beatazalewa <info@zalnet.pl>
2023-04-22 10:06:11 +0200

    Added YieldReturn project

---
+++ YieldReturn/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace YieldReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (char fib in Programmer("programista"))
+                Console.Write(fib + " ");
+
+            Console.ReadKey();
+        }
+
+        static IEnumerable<char> Programmer(string couplechars)
+        {
+            for (int i = 0; i < couplechars.Length; i++)
+            {
+                yield return couplechars[i];
+            }
+        }
+    }
+}
+
